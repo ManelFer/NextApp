ce17d72a38e28538ddcacf6d38eaddd4fb9247ac
manoel ferreira <95552207+ManelFer@users.noreply.github.com>
2025-02-10 21:37:14 -0300

    feat: create use and auth moudle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [AuthModule, UsersModule],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
